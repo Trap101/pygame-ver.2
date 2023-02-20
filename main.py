25de127201e3cb410e9ea2813f371ad6745d4df5
trap101 <cheng050929@gmail.com>
2023-02-20 14:58:18 +0000

    sdasdad

--- main.py
+++ main.py
@@ -36,23 +36,26 @@ class Slots:
         self.source = source
         self.name = name
         self.source = pygame.transform.scale(self.source,self.rect.size())
-        self.s_y = self.rect.y
+        self.s_y = 1000-self.rect.y
+        
         self.distance = distance
         self.trace_distance = 0
-        self.offset = 1000-self.s_y
     def calculate_pos(self,pt):
-        self.rect.y = (util.smoooth_step(0,self.distance,pt)%1000)+self.offset
+        self.rect.y = (util.overshoot_in_out(0,8000,pt,1.4,1.2)+self.s_y)%1200-200
+    def reset(self):
+        pass
     def render(self,surface:pygame.Surface):
-        surface.blit(self.source,self.rect.pos())
+        surface.blit(self.source,(self.rect.x,self.rect.y))
 class Reel:
     def __init__(self,items:dict) -> None:
         self.items = []
         
         for i,val in enumerate(list(items.items())):
-            self.items.append(Slots(Rect(0,1000-i*200,200,200),pygame.image.load(val[1]),name=val[0],distance=4500,trace_distance=0)) 
+            self.items.append(Slots(Rect(0,1000-i*200,200,200),pygame.image.load(val[1]),name=val[0],distance=4000,trace_distance=0)) 
     def render(self,surface:pygame.Surface,pt,dt):    
         ds = util.dsmooth_step_by_x(pt,0,300)*dt
         dss = ds
+        print(util.smoooth_step(0,4500,pt)%1200)
         # extra iteration in order for it to not spasm away
         # for i in range(int((ds//SPASM_CONSTANT)+1)):       
         #     clamped = util.clamp(0,SPASM_CONSTANT,dss)
@@ -62,9 +65,12 @@ class Reel:
         #         item.calculate_pos(clamped)
         for i, item in enumerate(self.items):
             item.calculate_pos(pt)
-        if self.items[0].rect.y>1000:
+        if self.items[0].rect.y>=1000:
+            print(True)
             t = self.items.pop(0)
-            self.items.append(Slots(Rect(0,-200-(1000-t.rect.y),200,200),t.source,name=t.name,distance=t.distance,trace_distance=t.trace_distance)) 
+            s = Slots(Rect(0,-200-(1000-t.rect.y),200,200),t.source,name=t.name,distance=t.distance,trace_distance=t.trace_distance)
+            s.s_y = t.s_y
+            self.items.append(s) 
         for i in self.items:
             i.render(surface)
 class Machine:
@@ -77,8 +83,8 @@ class Machine:
         self.running = True
         self.clock = pygame.time.Clock()
         self.dt = 0
-        self.t = 10
-        self.frame_cap = 1.0/60
+        self.t = 5
+        self.frame_cap = 1.0/80
         self.time1 = time.perf_counter()
         self.unprocessed = 0
         self.can_render = False
@@ -103,12 +109,12 @@ class Machine:
                     self.surface.fill(WHITE)
                     if self.st <= self.t:
                         self.reel.render(self.surface,self.st/self.t,self.f_p)
-                    else:
-                        print(self.t_exe)
                         for i,item in enumerate(self.reel.items):
                             print(i,item.rect.y,item.name)
-
-                        sys.exit()
+                    else:
+                        self.reel.render(self.surface,1,self.f_p)
+                        for i,item in enumerate(self.reel.items):
+                            print(i,item.rect.y,item.name)
                     # normal setting part
                     self.screen.fill(WHITE)
                     self.screen.blit(self.surface, (100, 100))
--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import math
 def lerp(x1,x2,t):
     return x1+(x2-x1)*t
 def smoooth_step(x1,x2,t):
@@ -6,3 +7,14 @@ def dsmooth_step_by_x(t, cx, dx):
     return cx+(dx-cx)*(6*t-6*t*t)
 def clamp(smallest,largest,n):
     return max(smallest,min(n,largest))
+def overshoot(x1,x2,t,elastic_constant:int):
+    return x1+(x2-x1)*(t*t*elastic_constant*(1-t)+((1-(1-t)*(1-t))*t))
+def plastic(x1,x2,t):
+    return x1+(x2-x1)*((pow(2,-10*t)*math.sin((10*t-0.75)*2*math.pi/3))+1)
+def overshoot_in_out(x1,x2,t,pl1,pl2):
+    """easing function
+    parameters:
+        pl1 :overshoot constantc
+        pl2,undershoot constant
+    """
+    return x1+(x2-x1)*(pl1*t*t*(1-t)+(1-(pl2*(1-t))*(pl2*(1-t)))*t)
