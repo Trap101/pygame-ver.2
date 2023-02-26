6ff04d3db80535c8558f2f9f57409a363da037bd
trap101 <cheng050929@gmail.com>
2023-02-26 15:39:17 +0000

    major changes

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import pygame
-import json
+import pygame,json,sys,time,util,menu
 from dataclasses import dataclass
-import sys
-import time
-import util
-WIDTH_SCREEN = 800
-HEIGHT_SCREEN = 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-SPASM_CONSTANT = 200
 pygame.init()
 # CENTRE_SQUARE = (100, 100, 600, 400)
 # screen = pygame.display.set_mode([WIDTH_SCREEN, HEIGHT_SCREEN])
@@ -16,46 +9,30 @@ pygame.init()
 # IMAGESCALE = (CENTRE_SQUARE[-2]/3, CENTRE_SQUARE[-1]/3)
 # clock = pygame.time.Clock()
 # dt = 0
-# drawsurface = pygame.Surface((600, 400))
-@dataclass
-class Rect:
-    x:float
-    y:float
-    width:float
-    height:float
-    
-    def convert(self):
-        return (self.x,self.y,self.width,self.height)
-    def pos(self):
-        return (self.x,self.y)
-    def size(self):
-        return (self.width,self.height)
+# drawsurface = pygame.Surface((600, 400))\
 class Slots:
-    def __init__(self,rect:Rect,source:pygame.surface.Surface,name,distance,trace_distance) -> None:
+    def __init__(self,rect:pygame.Rect,source:pygame.surface.Surface,name,distance,trace_distance) -> None:
         self.rect = rect 
         self.source = source
         self.name = name
-        self.source = pygame.transform.scale(self.source,self.rect.size())
+        self.source = pygame.transform.scale(self.source,(self.rect.w,self.rect.h))
         self.s_y = 1000-self.rect.y
         
         self.distance = distance
         self.trace_distance = 0
     def calculate_pos(self,pt):
-        self.rect.y = (util.overshoot_in_out(0,8000,pt,1.4,1.2)+self.s_y)%1200-200
+        self.rect.y = (util.overease(0,8000,pt)+self.s_y)%1200-200
     def reset(self):
         pass
     def render(self,surface:pygame.Surface):
         surface.blit(self.source,(self.rect.x,self.rect.y))
 class Reel:
-    def __init__(self,items:dict) -> None:
+    def __init__(self,items:dict,x) -> None:
         self.items = []
-        
+        self.x = x 
         for i,val in enumerate(list(items.items())):
-            self.items.append(Slots(Rect(0,1000-i*200,200,200),pygame.image.load(val[1]),name=val[0],distance=4000,trace_distance=0)) 
+            self.items.append(Slots(pygame.Rect(x,1000-i*200,200,200),pygame.image.load(val[1]),name=val[0],distance=4000,trace_distance=0)) 
     def render(self,surface:pygame.Surface,pt,dt):    
-        ds = util.dsmooth_step_by_x(pt,0,300)*dt
-        dss = ds
-        print(util.smoooth_step(0,4500,pt)%1200)
         # extra iteration in order for it to not spasm away
         # for i in range(int((ds//SPASM_CONSTANT)+1)):       
         #     clamped = util.clamp(0,SPASM_CONSTANT,dss)
@@ -65,39 +42,38 @@ class Reel:
         #         item.calculate_pos(clamped)
         for i, item in enumerate(self.items):
             item.calculate_pos(pt)
-        if self.items[0].rect.y>=1000:
-            print(True)
-            t = self.items.pop(0)
-            s = Slots(Rect(0,-200-(1000-t.rect.y),200,200),t.source,name=t.name,distance=t.distance,trace_distance=t.trace_distance)
-            s.s_y = t.s_y
-            self.items.append(s) 
+        # if self.items[0].rect.y>=1000:
+        #     t = self.items.pop(0)
+        #     s = Slots(Rect(0,-200-(1000-t.rect.y),200,200),t.source,name=t.name,distance=t.distance,trace_distance=t.trace_distance)
+        #     s.s_y = t.s_y
+        #     self.items.append(s) 
         for i in self.items:
             i.render(surface)
 class Machine:
-    def __init__(self,window_size,main_square,items:dict) -> None:
-        self.screen = pygame.display.set_mode(window_size) 
-        self.surface= pygame.Surface(main_square)
-        self.reel = Reel(items)
+    def __init__(self,setting:dict) -> None:
+        items = setting["items"]
+        self.moving = False
+        self.screen = pygame.display.set_mode(setting["window_size"]) 
+        self.surface= pygame.Surface(setting["main_square"])
+        self.reels = [Reel(items,200*(setting["reel_num"]-i-1)) for i in range(setting["reel_num"])]
         self.items = []
         self.i_count = len(items.keys())
         self.running = True
         self.clock = pygame.time.Clock()
         self.dt = 0
-        self.t = 5
-        self.frame_cap = 1.0/80
+        self.t = 10
+        self.frame_cap = 1.0/59
         self.time1 = time.perf_counter()
         self.unprocessed = 0
-        self.can_render = False
         self.st = 0
-        self.t_exe = 0
         self.f_p = 0
+        self.button = menu.button(pygame.rect.Rect(775,300,150,100))
     def run_game(self):
         while self.running is True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
             if pygame.display.get_init():
-                self.can_render = False 
                 time_2 = time.perf_counter()
                 passed = time_2-self.time1
                 self.st+=passed
@@ -105,22 +81,22 @@ class Machine:
                 self.unprocessed += passed
                 self.time1 = time_2
                 while self.unprocessed >= self.frame_cap:
-                    self.t_exe+=1 
                     self.surface.fill(WHITE)
                     if self.st <= self.t:
-                        self.reel.render(self.surface,self.st/self.t,self.f_p)
-                        for i,item in enumerate(self.reel.items):
-                            print(i,item.rect.y,item.name)
+                        for i,reel in enumerate(self.reels): 
+                            pass
+                            reel.render(self.surface,self.st/self.t,self.f_p)
                     else:
-                        self.reel.render(self.surface,1,self.f_p)
-                        for i,item in enumerate(self.reel.items):
-                            print(i,item.rect.y,item.name)
+                        for i, reel in enumerate(self.reels):
+                            pass
+                            reel.render(self.surface,1,self.f_p)
                     # normal setting part
-                    self.screen.fill(WHITE)
+                    self.screen.fill(WHITE) 
                     self.screen.blit(self.surface, (100, 100))
+                    self.button.render(self.screen,5)
+                    # pygame.draw.rect(self.screen,BLACK,(800,500,300,300))
                     # done
                     pygame.display.flip()
-
                     self.unprocessed -= self.frame_cap
                     self.f_p = 0
     # Done! Time to quit.
@@ -133,10 +109,10 @@ class Machine:
 
 
 def init_with_setting(filename):
-    with open(filename) as f:
+    with open(filename,"r") as f:
         data = json.load(f)
-    m = Machine(window_size=data["window_size"],main_square=data["main_square"],items=data["items"])
+    m = Machine(data)
     m.run_game()
-
-init_with_setting("setting.json")
+if __name__ == "__main__": 
+    init_with_setting("setting.json")
     
--- util.py
+++ util.py
@@ -1,4 +1,32 @@
 import math
+from dataclasses import dataclass
+import pygame
+        
+@dataclass
+class Vector2:
+    x:float
+    y:float
+    
+@dataclass
+class Rect:
+    x:float
+    y:float
+    width:float
+    height:float
+    
+    def __iter__(self):
+        for i in self.__dict__:
+            yield self.__dict__[i]
+    def convert(self):
+        return (self.x,self.y,self.width,self.height)
+    def pos(self):
+        return (self.x,self.y)
+    def size(self):
+        return (self.width,self.height)
+    def collision_point(self,destination:Vector2):
+        return (destination.x > self.x and destination.x < self.x+self.width and destination.y > self.y and self.y <self.y+self.height)
+BLACK = (0,0,0)
+WHITE= (255,255,255)
 def lerp(x1,x2,t):
     return x1+(x2-x1)*t
 def smoooth_step(x1,x2,t):
@@ -12,9 +40,20 @@ def overshoot(x1,x2,t,elastic_constant:int):
 def plastic(x1,x2,t):
     return x1+(x2-x1)*((pow(2,-10*t)*math.sin((10*t-0.75)*2*math.pi/3))+1)
 def overshoot_in_out(x1,x2,t,pl1,pl2):
-    """easing function
-    parameters:
-        pl1 :overshoot constantc
-        pl2,undershoot constant
+    """Returns the overshoot ease in and out function
+    Parameters:
+        pl1 (int):overshoot constant,
+        pl2 (int):undershoot constant.
+    Returns:
+        the returning of a overshoot function.
     """
     return x1+(x2-x1)*(pl1*t*t*(1-t)+(1-(pl2*(1-t))*(pl2*(1-t)))*t)
+def overease(x1,x2,t):
+    return(x1+(x2-x1)*(1-((1-t)**6)))
+def draw_outlined_square(surface,rect:tuple,color:tuple,border_size:int,border_color=None):
+    if border_color is None:
+        border_color = color
+    pygame.draw.rect(surface,color,(800,500,100,100))
+    pygame.draw.rect(surface,border_color,rect,border_size)
+
+ 
